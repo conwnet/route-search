282bc931b1e9115e2d216b99814dcfffc7aea6f3
netcon <netcon@live.com>
2016-09-13 00:39:34 +0800

    20160913

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -10,63 +10,71 @@ const int maxn = 8000;
 Time dis[8000];
 int prev[maxn];
 
-void show(Graph *G, int to, int deep)
+char time_str[80];
+void show(Graph *G, int to)
 {
-	if(deep == 20) return ;
-	printf("%s\n", G->stop_name[to]);
-	if(prev[to] != -1) show(G, prev[to], deep + 1);
+    printf("%s %s\n", G->stop_name[to], dis[to].tostr(time_str));
+    if(prev[to] != -1) show(G, prev[to]);
 }
 
+int used[8000];
 int Dijkstra(Graph *G, int from, int to, const char *time)
 {
-	priority_queue<pti, vector<pti>, greater<pti> > pq;
-	for(int i = 0; i < maxn; i++)
-		dis[i].inf();
-	memset(prev, -1, sizeof(prev));
-	pq.push(pti(time, from));
-	dis[from] = time;
-	while(!pq.empty()) {
-		pti p = pq.top(); pq.pop();
-		int u = p.second;
-
-		if(p.second == to) {
-			break;
-		}
-		if(dis[u] < p.first)
-			continue;
-		//printf("%s\n", G->stop_name[u]);
-		for(int i = G->trip_head[u]; i != -1; i = G->trip[i].next) {
-			Trip &e = G->trip[i];
-			Time ti = dis[u].reset(e.start);
-			//if(ti - dis[u] > 86400) continue;
-			if(ti + e.cost < dis[e.to]) {
-				dis[e.to] = ti + e.cost;
-				//if(e.to == from) printf("error!\n");
-				prev[e.to] = u;
-				pq.push(pti(dis[e.to], e.to));
-			}
-		}
+    priority_queue<pti, vector<pti>, greater<pti> > pq;
+    for(int i = 0; i < maxn; i++)
+        dis[i].inf();
+    memset(prev, -1, sizeof(prev));
+    memset(used, 0, sizeof(used));
+    pq.push(pti(time, from));
+    dis[from] = time;
+    while(!pq.empty()) {
+        pti p = pq.top(); pq.pop();
+        int u = p.second;
+        
+        used[u] = 1;
+        if(p.second == to) {
+        //    show(G, to);
+            break;
+        }
+        if(dis[u] < p.first)
+            continue;
+        //printf("%s\n", G->stop_name[u]);
+        for(int i = G->trip_head[u]; i != -1; i = G->trip[i].next) {
+            Trip &e = G->trip[i];
+            Time ti = dis[u].reset(e.start);
+            //if(ti - dis[u] > 1200) continue;
+            if(!used[e.to] && ti + e.cost < dis[e.to]) {
+                dis[e.to] = ti + e.cost;
+                prev[e.to] = u;
+                pq.push(pti(dis[e.to], e.to));
+            }
+        }
 /**
-		for(int i = G->trip_head[u]; i != -1; i = G->transfer[i].next) {
-			Transfer &e = G->transfer[i];
-			//if(
-		}
+        for(int i = G->trip_head[u]; i != -1; i = G->transfer[i].next) {
+            Transfer &e = G->transfer[i];
+            //if(
+        }
 */
-	}
-	
-	//show(G, to, 0);
-	printf("%s\n", ctime(&dis[to].timer));
-	
+    }
+    
+    //show(G, to, 0);
+    printf("%s\n", ctime(&dis[to].timer));
+    
 }
 
 int main()
 {
-	int c = clock();
-	Graph *G = new Graph(5000, 200000, 10000);
-	read_files(G);
-	printf("%d\n", clock() - c);
-	Dijkstra(G, 1185, 472, "2016-07-08 12:00:00");
-	
+    int c = clock();
+    Graph *G = new Graph(5000, 200000, 10000);
+    read_files(G);
+    Dijkstra(G, 1651, 472, "2016-07-08 8:00:00");
+    printf("%d\n", clock() - c);
+/**
+    Time b = "2016-07-08 12:00:00";
+    Time t = Time(b.timer).reset(124);
+    char s[80];
+    printf("%d %s\n", t.timer, t.tostr(s));
+*/    
 
-	return 0;
+    return 0;
 }
--- algorithm/route_time.h
+++ algorithm/route_time.h
@@ -1,6 +1,6 @@
 /**
 
-	a simple time class to deal the time problem in the algorithm
+    a simple time class to deal the time problem in the algorithm
 
 */
 
@@ -14,126 +14,124 @@
 #include <time.h>
 
 struct Time {
-	time_t timer;
-	int year, month, day, hour, minute, second;
+    time_t timer;
+    int year, month, day, hour, minute, second;
 
-	/// default all 0
-	Time() {
-		year = month = day = hour = minute = second = 0;
-	}
+    /// default all 0
+    Time() {
+        year = month = day = hour = minute = second = 0;
+    }
 
-	Time(time_t timer) {
-		this->timer = timer;
-		tm *p = localtime(&timer);
-		year = p->tm_year + 1900;
-		month = p->tm_mon + 1;
-		day = p->tm_mday;
-		hour = p->tm_hour;
-		minute = p->tm_min;
-		second = p->tm_sec;
-	}
+    Time(time_t timer) {
+        this->timer = timer;
+        tm *p = localtime(&timer);
+        year = p->tm_year + 1900;
+        month = p->tm_mon + 1;
+        day = p->tm_mday;
+        hour = p->tm_hour;
+        minute = p->tm_min;
+        second = p->tm_sec;
+    }
 
-	/// structure time use string like "2016-07-27 21:00:00"
-	Time(const char *time_format_string) {
-		year = atoi(time_format_string);
-		month = atoi(time_format_string + 5);
-		day = atoi(time_format_string + 8);
-		hour = atoi(time_format_string + 11);
-		minute = atoi(time_format_string + 14);
-		second = atoi(time_format_string + 17);
-		update();
-	}
+    /// structure time use string like "2016-07-27 21:00:00"
+    Time(const char *time_format_string) {
+        year = atoi(time_format_string);
+        month = atoi(time_format_string + 5);
+        day = atoi(time_format_string + 8);
+        hour = atoi(time_format_string + 11);
+        minute = atoi(time_format_string + 14);
+        second = atoi(time_format_string + 17);
+        update();
+    }
 
-	void update() {
-		tm time;
-		time.tm_year = year - 1900;
-		time.tm_mon = month - 1;
-		time.tm_mday = day;
-		time.tm_hour = hour;
-		time.tm_min = minute;
-		time.tm_sec = second;
-		timer = mktime(&time);
-	}
+    void update() {
+        tm time;
+        time.tm_year = year - 1900;
+        time.tm_mon = month - 1;
+        time.tm_mday = day;
+        time.tm_hour = hour;
+        time.tm_min = minute;
+        time.tm_sec = second;
+        timer = mktime(&time);
+    }
 
-	/// reset time
-	Time reset(int day_sec) {
-		Time ti = Time(timer);
-		if(day_sec / 3600 < ti.hour) ti.day += 1;
-		ti.hour = day_sec / 3600; day_sec %= 3600;
-		ti.minute = day_sec / 60; day_sec %= 60;
-		ti.second = day_sec;
-		ti.update();
-		return ti;
-	}
-	
-	/// copy structure
-	Time(const Time &time) {
-		hour = time.hour;
-		minute = time.minute;
-		second = time.second;
-		update();
-	}
+    /// reset time
+    Time reset(int day_sec) {
+        time_t dt = hour * 3600 + minute * 60 + second;
+        time_t ret = timer - dt + day_sec;
+        if(day_sec < dt)
+            ret += 86400;
+        return Time(ret);
+    }
+    
+    /// copy structure
+    Time(const Time &time) {
+        hour = time.hour;
+        minute = time.minute;
+        second = time.second;
+        update();
+    }
 
-	/// maximize the time
-	void inf() {
-		year = 9999;
-		month = 12;
-		day = 31;
-		hour = 23;
-		minute = 59;
-		second = 59;
-		update();
-	}
+    /// maximize the time
+    void inf() {
+        year = 9999;
+        month = 12;
+        day = 31;
+        hour = 23;
+        minute = 59;
+        second = 59;
+        update();
+    }
 
-	/// make the < operator
-	bool operator< (const Time &time) const {
-		return timer < time.timer;
-	}
+    /// make the < operator
+    bool operator< (const Time &time) const {
+        return timer < time.timer;
+    }
 
-	/// make the = operator with a format string
-	Time operator= (const char *time_format_string) {
-		if(strchr(time_format_string, '-')
-			&& atoi(time_format_string) != 0) {
-			timer = Time(time_format_string).timer;
-		} else {
-			year = month = day = 0;
-			hour = atoi(time_format_string);
-			minute = atoi(time_format_string + 3);
-			second = atoi(time_format_string + 6);
-		}
-	}
-
-	
-/***
-	/// useless because the copy structrue function
-	/// make the = operator with a Time object
-	Time operator= (const Time &time) {
-		year = time.year;
-		month = time.month;
-		day = time.day;
-		hour = time.hour;
-		minute = time.minute;
-		second = time.second;
-	}
+/**
+    /// make the = operator with a format string
+    Time operator= (const char *time_format_string) {
+        if(strchr(time_format_string, '-')
+            && atoi(time_format_string) != 0) {
+            *this = Time(time_format_string);
+        } else {
+            year = month = day = 0;
+            hour = atoi(time_format_string);
+            minute = atoi(time_format_string + 3);
+            second = atoi(time_format_string + 6);
+        }
+    }
 */
+    
+    /// useless because the copy structrue function
+    /// make the = operator with a Time object
+    Time operator= (const Time &time) {
+        timer = time.timer;
+        year = time.year;
+        month = time.month;
+        day = time.day;
+        hour = time.hour;
+        minute = time.minute;
+        second = time.second;
+    }
 
-	///make the - opeartor
-	int operator- (const Time &time) {
-		return timer - time.timer;
-	}
+    ///make the - opeartor
+    int operator- (const Time &time) {
+        return timer - time.timer;
+    }
 
-	///make the - opeartor
-	Time operator+ (const int interval) {
-		int t = timer + interval;
-		return Time(timer);
-	}
+    ///make the - opeartor
+    Time operator+ (const int interval) {
+        int t = timer + interval;
+        return Time(timer);
+    }
 
-	/// save as a string
-	char *tostr(char *str) {
-		sprintf(str, "%04d-%02d-%02d %02d:%02d:%02d",
-			year, month, day, hour, minute, second);
-		return str;
-	}
+    /// save as a string
+    char *tostr(char *str) {
+        sprintf(str, "%04d-%02d-%02d %02d:%02d:%02d",
+            year, month, day, hour, minute, second);
+        return str;
+    }
 };
 
 //typedef struct Time Time;
